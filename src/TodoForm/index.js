616fbb534e87224115e8e72b4d43cf50587af484
andresfls-buc <andresflsxx@gmail.com>
2024-09-07 18:12:15 +0900

    Addiciion de la funcionalida del boton Cancel

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,29 +1,40 @@
 import React from "react";
 import "./TodoForm.css";
+import { TodoContext } from "../TodoContext";
 
 function TodoForm() {
+  const {
+    // addTodo
+    setOpenModal,
+  } = React.useContext(TodoContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setOpenModal(false);
+  };
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      <label>Escribe tu nuevo TODO</label>
-      <textarea placeholder=" escribe" />
+    <form onSubmit={onSubmit}>
+      <label>Create your new TODO'S</label>
+      <textarea placeholder=" Write your new TODO's" />
       <button
-        type=""
+        type="button"
         className="TodoForm-button
             TodoForm-button--cancel"
+        onClick={onCancel}
       >
         {" "}
-        Cancelar
+        Cancel
       </button>
       <button
         type="submit"
         className="TodoForm-button
             TodoForm-button--add"
       >
-        Anadir
+        Add
       </button>
     </form>
   );
